f8ccf92bf4fa17f7e53548db4ea93722bfcf8736
Quang <quangerdude@gmail.com>
2013-03-20 17:17:22 -0700

    added editcontact php and html

---
+++ wp-content/themes/flare-child-theme/edit_contact2.php
@@ -0,0 +1,62 @@
+<?php
+/*
+ * Template Name: Edit Contact 2
+ *
+ */
+?>
+
+<?php get_header(); ?>
+
+	<?php  the_post(); ?>
+
+		<?php get_template_part( 'precontent' ); ?>
+
+<div id="content">
+
+		<div id="content-inner" class="<?php echo btp_content_get_class(); ?>">
+
+			<div class="grid">
+				<!--
+				<article id="post-<?php the_ID(); ?>" <?php post_class( 'c-three-fourth' ); ?>>									
+
+					<div class="entry-content">	
+
+						<?php include ('custompages/editcontact.html'); //the_content() ?>
+
+						<?php btp_wp_link_pages(); ?>
+
+					</div><!- .entry-content -->
+					<!--
+					<div class="entry-utility">		
+
+						<?php edit_post_link( __( 'Edit', 'btp_theme' ), '<span class="edit-link">', '</span>' ); ?>
+
+					</div><!- .entry-utility -->
+				<!--
+				</article> -->
+
+				<?php include ('custompages/editcontact.html'); ?>
+
+				<aside class="c-one-fourth sidebar after">
+
+					<div class="helper"></div>
+
+					<div class="inner">
+
+						<?php get_template_part( 'page_nav' ); ?>
+
+					</div>
+
+					<div class="helper"></div>
+
+				</aside>
+
+			</div>		
+
+		</div><!-- #content-inner -->
+
+		<div class="background"><div></div></div>
+
+	</div><!-- #content -->
+	
+<?php get_footer(); ?>
