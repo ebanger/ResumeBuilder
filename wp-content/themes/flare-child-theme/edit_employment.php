f3d0fc1a44b085a1290e96f2bc18921081db89c3
Sandra Howard <shhoward@asu.edu>
2013-04-02 11:43:34 -0700

    Update edit_employment.php

--- wp-content/themes/flare-child-theme/edit_employment.php
+++ wp-content/themes/flare-child-theme/edit_employment.php
@@ -230,7 +230,24 @@
 
 
 
-	
+$link = mysql_connect('localhost', 'themaro0_dev1', 'buildaresume!1');
+if (!link) {
+die('Could not connect: ' . mysql_error());
+}
+
+$db_selected = mysql_select_db('themaro0_dev1', $link);
+if (!db_selected){
+die('Cant use DB: ' . mysql_error());
+}
+
+$result = json_decode($o);
+foreach($result as $key => $value) {
+    if($value) {
+
+            //how to use json array to insert data in Database
+        mysql_query("INSERT INTO tablename (company, Title, city, State, country) VALUES ($value->company, $value->Title,$value->city, $value->State, $value->country)");
+    }
+    mysql_close($link);
 
 
 
