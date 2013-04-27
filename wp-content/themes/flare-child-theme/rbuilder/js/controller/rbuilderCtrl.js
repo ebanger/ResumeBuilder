0a231cea16ca196cd7f77356407a257f16820df3
Quang Le <quangerdude@gmail.com>
2013-04-27 00:39:06 -0700

    updated view and index

--- wp-content/themes/flare-child-theme/rbuilder/js/controller/rbuilderCtrl.js
+++ wp-content/themes/flare-child-theme/rbuilder/js/controller/rbuilderCtrl.js
@@ -4,8 +4,11 @@
 
 rbuildermvc.controller('rbuilderCtrl', function rbuilderCtrl($scope, rbuilderStorage) {
 	// do stuff
-	var resumeList = $scope.resumeList = {"resumes":[{"resumeID":"1","resumeName":"Java Developer","dateModified":"2013-23-4"},{"resumeID":"2","resumeName":"Pizza Maker","dateModified":"2010-22-6"},{"resumeID":"3","resumeName":"Astronaut","dateModified":"2011-14-1"},{"resumeID":"4","resumeName":"Linux Administrator","dateModified":"2013-01-12"}]};
-	var resume = $scope.resume = rbuilderStorage.getResume(2);;
+	//var resumeList = $scope.resumeList = {"resumes":[{"resumeID":"1","resumeName":"Java Developer","dateModified":"2013-23-4"},{"resumeID":"2","resumeName":"Pizza Maker","dateModified":"2010-22-6"},{"resumeID":"3","resumeName":"Astronaut","dateModified":"2011-14-1"},{"resumeID":"4","resumeName":"Linux Administrator","dateModified":"2013-01-12"}]};
+	var resumeList = $scope.resumeList = rbuilderStorage.getResumeList(2);
+	var resume = $scope.resume;
+	var currentUser;
+	var currentResumeId;
 
 	$scope.getResumeList = function(userID){
 		resumeList = rbuilderStorage.getResumeList(userID);
@@ -15,19 +18,25 @@ rbuildermvc.controller('rbuilderCtrl', function rbuilderCtrl($scope, rbuilderSto
 
 	$scope.saveResume = function(){
 		resume = $scope.resume;
-		console.log($scope.resume)
-		rbuilderStorage.update(resume, 3);
+		console.log(resume);
+		rbuilderStorage.update(resume, parseInt(resume.currentResumeId, 10));
 	}
 
-	$scope.getResume = function(resumeID){
-	 	resume = rbuilderStorage.getResume(resumeID);
+	$scope.getResume = function(index){
+		currentResumeId = parseInt(index, 10);
+		//currentResumeId = currentResumeId - 1;
+	 	resume = rbuilderStorage.getResume(currentResumeId, 10);
 	 	$scope.resume = resume;
+	 	//window.location.href = "http://localhost/ResumeBuilder/wp-content/themes/flare-child-theme/rbuilder/index.html";
+		console.log(resume);
 	}
 
-
-	/* Phien: list of functions */
-	$scope.addName = function() {
-
+	$scope.deleteResume = function(index) {
+		if(confirm("Are you sure you want to delete this resume?")){
+			var resumeToDelete = parseInt(index, 10);
+			rbuilderStorage.deleteResume(parseInt(resumeList.resumes[resumeToDelete].resumeID, 10));
+			resumeList = $scope.resumeList = rbuilderStorage.getResumeList(2);
+		}
 	}
 
 	$scope.addSchool = function() {
